9b986ec21fa8e0af263c0259838e934ece553cd3
iPNXx <pekfnd@gmail.com>
2022-05-27 19:06:31 -0300

    criação dos models

--- notas/notas/settings.py
+++ notas/notas/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'nota.apps.NotaConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
